3ffbb16e77ebd725795c79026a030c148610aef9
Marcos <marcos.fuenmayorhtc@gmail.com>
2021-03-28 19:37:04 -0400

    nav feature

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import Welcome from './src/pages/welcome';
-import Login from './src/pages/Login';
-import Register from './src/pages/Register';
+import React from "react";
+import { Route } from "react-router";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Welcome from "./src/pages/welcome";
+
+import Login from "./src/pages/Login";
+import Register from "./src/pages/Register";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <Welcome></Welcome>
+    <BrowserRouter>
+      <Route path="/login" render={() => <Login />}></Route>
+      <Route path="/register" render={() => <Register />}></Route>
+      <Route path="/welcome" render={() => <Welcome />}></Route>
+      <Route path="/" render={() => <Welcome />}></Route>
+    </BrowserRouter>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  }
+    backgroundColor: "#fff",
+  },
 });
