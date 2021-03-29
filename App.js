e37cfdcf8dd3f33f8465483ca2786cf2414b1fe8
Blue01H <juandiegourdanetafuenmayor@gmail.com>
2021-03-28 20:48:24 -0400

    Dashboard 1.0

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Welcome from './src/pages/welcome';
 import Login from './src/pages/Login';
 import Register from './src/pages/Register';
+import Dashboard from './src/pages/Dashboard'
 
 export default function App() {
   return (
-    <Welcome></Welcome>
+    <Dashboard></Dashboard>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
+
+class Dashboard extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <View style={styles.avatarContainer}>
+                    
+                    <Image
+                        source={{ uri: 'https://www.shankarainfra.com/img/avatar.png', }}
+                        style={styles.avatar}
+                    />
+
+                    <View style={{paddingLeft: 10}}>
+                        <TextInput 
+                           placeholder="Search"
+                           placeholderTextColor = "#888"
+                           style={styles.searchBar}
+                        />
+                    </View>
+                
+               
+
+
+                </View>
+
+            </View>
+        );
+    }
+} export default Dashboard;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    avatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 150 / 2,
+        overflow: "hidden",
+        borderWidth: 1,
+        borderColor: '#888',
+    },
+    avatarContainer: {
+        paddingLeft: 8,
+        paddingTop: 30,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    searchBar: {
+        backgroundColor: '#f0f0f0',
+        borderRadius: 8,
+        height: 45,
+        width: 320,
+        color: '#000',
+        paddingLeft: 18
+    }
+})
